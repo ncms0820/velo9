7d9213c5badb6b42d026def44f53f2426c4bbbb8
Minsu Cho <ncms0820@outlook.com>
2022-04-08 20:05:39 +0900

    fixed homepage

--- src/views/homepage/home.jsx
+++ src/views/homepage/home.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 import Card from "../../components/card/card";
 import Header from "../header/header";
 import styles from "./home.module.scss";
 import Nav from "./nav";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-const Home = (props) => (
-  <div className={styles.container}>
-    <Header />
-    <Nav />
-    <section className={styles.grid_container}>
-      <Card />
-    </section>
-  </div>
-);
+const Home = (props) => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Nav />
+      <section className={styles.grid_container}>
+        <Card />
+      </section>
+    </div>
+  );
+};
 
 export default Home;
