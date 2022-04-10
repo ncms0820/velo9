0677d2bc8ace4b5d9a13667170a8454f86bc1724
pie <cpie1216@gamil.com>
2022-04-10 16:53:08 +0900

    무한스크롤 도입, 테스트

--- src/views/login/Test.jsx
+++ src/views/login/Test.jsx
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 // import { pagination } from '../src/service/InfiniteScroll'
 import Card from "../../components/card/card";
+import InfiniteScroll from "../../service/InfiniteScroll";
 
 
 // const testDb = new Array(100).fill(0).map( (v, i) => i+1).reverse(); // 간단 테스트용 array, 100~1까지
 
 const Test = ( { dbService, authService }) => {
 
+  // 유저정보 설정
   const testDb = dbService.getDb("new").content
   const navigate = useNavigate();
   const navigateState = useLocation().state; // firebase => 유저가 로그인되면 세션(비슷한거, 헤더정보)에 저장해줌.
   const [userId, setUserId] = useState(navigateState && navigateState.id);
 
+  // DB 받아오기 위한 설정값
   const [lastId, setLastId] = useState(0) // 가져온 id 저장
   const [posts, setPosts] = useState([])
   const size = 5; // 한번에 가져올 갯수
+
+  // 무한스크롤 하기 위한 Ref
   
   const pagination = () => {
     const targetPost = testDb.slice(lastId, lastId + 5)
@@ -40,6 +45,10 @@ const Test = ( { dbService, authService }) => {
   }, [userId, authService, navigate]);
   
   
+  // const observerRef = useRef(); // 옵저버로 지정할 ref
+  const lastListRef = useRef(null); // 마지막 요소
+  InfiniteScroll(lastListRef, posts, pagination)
+  
   return (
     <>
     테스트페이지
@@ -48,7 +57,7 @@ const Test = ( { dbService, authService }) => {
 
     { posts&&
       posts.map((v, i) => {
-      return(<Card key={i} content={v} />)
+      return( <Card key={i} content={v} ref={ i === posts.length-1 ? lastListRef : null} />)
     })}
     </>
   )
