6edddcee3eb5144422dc42efa4f97e4f1d794e6f
pie <cpie1216@gamil.com>
2022-04-11 21:53:35 +0900

    merge

--- src/views/login/Test.jsx
+++ src/views/login/Test.jsx
@@ -6,7 +6,6 @@ import Card from "../../components/card/card";
 
 // const testDb = new Array(100).fill(0).map( (v, i) => i+1).reverse(); // 간단 테스트용 array, 100~1까지
 
-<<<<<<< HEAD
 const Test = ( { dbService, authService }) => {
 
   const testDb = dbService.getDb("new").content
@@ -15,24 +14,14 @@ const Test = ( { dbService, authService }) => {
   const [userId, setUserId] = useState(navigateState && navigateState.id);
 
   const [lastId, setLastId] = useState(0) // 가져온 id 저장
-=======
-
-  const [lastId, setLastId] = useState(testData.length) // 가져온 id 저장
->>>>>>> 5c28fcfad62123fe28dfc79789be683a6ad1d99b
   const [posts, setPosts] = useState([])
   const size = 5; // 한번에 가져올 갯수
   
   const pagination = () => {
-<<<<<<< HEAD
-    const targetPost = testDb.slice(lastId, lastId + 5)
-    setPosts([...posts, ...targetPost])
-    setLastId(lastId + size) // 반영이 안되네; 
-=======
-    const targetIdx = testData.indexOf(lastId)
-    const targetPost = testData.slice(targetIdx, targetIdx + size)
+    const targetIdx = testDb.indexOf(lastId)
+    const targetPost = testDb.slice(targetIdx, targetIdx + size)
     setPosts([...posts, ...targetPost])
     setLastId(lastId - size) // 반영이 안되네; 
->>>>>>> 5c28fcfad62123fe28dfc79789be683a6ad1d99b
   }
 
   useEffect(() => {
