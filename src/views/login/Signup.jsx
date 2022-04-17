b4ed46ab9989f02e8b0bf0ba91e3e36b0ace3d55
Minsu Cho <ncms0820@outlook.com>
2022-04-17 14:37:02 +0900

    pre pull

--- src/views/login/Signup.jsx
+++ src/views/login/Signup.jsx
@@ -11,24 +11,16 @@ import DoubleCheckPw from "./pageCompnents/DoubleCheckPw";
 import CheckId from "./pageCompnents/CheckId";
 import CheckNickName from "./pageCompnents/CheckNickName";
 
-const Signup = ( { page, setPage, setOnLoginModal, authService } ) => {
+const Signup = ({ page, setPage, setOnLoginModal, authService }) => {
+  const [email, setEmail] = useState("");
+  const [id, setId] = useState(""); // 아이디 입력\
+  const [newPw, setNewPw] = useState(""); // 비밀번호
+  const [nickName, setNickName] = useState(""); // 닉네임
 
-  const [email, setEmail] = useState('')
-  const [id, setId] = useState('') // 아이디 입력\
-  const [newPw, setNewPw] = useState('') // 비밀번호 
-  const [nickName, setNickName] = useState('') // 닉네임
-
-  const [isCheckedId, setIsCheckedId] = useState(null) // null || boolean, 아이디 중복확인여부
-  const [isVerified, setIsVerified] = useState(null) // null || boolean, 인증번호 확인여부
-  const [isCheckedPw, setIsCheckedPw] = useState(null) // null || boolean, 비밀번호 체크여부
-  const [isCheckedNickName, setIsCheckedNickName] = useState(null) // null || boolean, 비밀번호 체크여부
-
-  const checkNickName = async () => {
-    console.log(nickName)
-    const checkNick = await authService.validateNickname(nickName)
-    setIsCheckedNickName(checkNick)
-  }
-
+  const [isCheckedId, setIsCheckedId] = useState(null); // null || boolean, 아이디 중복확인여부
+  const [isVerified, setIsVerified] = useState(null); // null || boolean, 인증번호 확인여부
+  const [isCheckedPw, setIsCheckedPw] = useState(null); // null || boolean, 비밀번호 체크여부
+  const [isCheckedNickName, setIsCheckedNickName] = useState(null); // null || boolean, 비밀번호 체크여부
 
   const finishSignup = () => {
     if (!isCheckedId) {
@@ -47,46 +39,43 @@ const Signup = ( { page, setPage, setOnLoginModal, authService } ) => {
       alert("닉네임 확인이 필요합니다. 일치하지 않습니다.");
       return;
     }
-    console.log("가입 완료") //가입로직
+    console.log("가입 완료"); //가입로직
     // 가입
-    authService.signup(id, newPw, nickName, email)
+    authService.signup(id, newPw, nickName, email);
     // setOnLoginModal(false)
-  }
+  };
 
-
-  return(
-      <>
+  return (
+    <>
       {/* 이메일 확인 */}
-        <VerifyEmail
-          email={email} setEmail={setEmail}
-          setIsVerified={setIsVerified}
-          isVerified={isVerified}
-          authService={authService}
-        />
+      <VerifyEmail
+        email={email}
+        setEmail={setEmail}
+        setIsVerified={setIsVerified}
+        isVerified={isVerified}
+        authService={authService}
+      />
 
       {/* 아이디 중복체크 */}
-        <CheckId
-          id={id} setId={setId}
-          isCheckedId={isCheckedId}
-          setIsCheckedId={setIsCheckedId}
-          authService={authService}
-        /> 
+      <CheckId
+        id={id}
+        setId={setId}
+        isCheckedId={isCheckedId}
+        setIsCheckedId={setIsCheckedId}
+        authService={authService}
+      />
 
       {/* 닉네임 중복체크 */}
-        <CheckNickName
-          nickName={nickName} setNickName={setNickName}
-          isCheckedNickName={isCheckedNickName}
-          setIsCheckedNickName={setIsCheckedNickName}
-          authService={authService}
-        />
+      <CheckNickName
+        nickName={nickName}
+        setNickName={setNickName}
+        isCheckedNickName={isCheckedNickName}
+        setIsCheckedNickName={setIsCheckedNickName}
+        authService={authService}
+      />
 
       {/* 비밀번호체크, async X */}
-        <DoubleCheckPw
-          newPw={newPw} setNewPw={setNewPw}
-          isCheckedPw={isCheckedPw}
-          setIsCheckedPw={setIsCheckedPw}
-        />
-
+      <DoubleCheckPw newPw={newPw} setNewPw={setNewPw} isCheckedPw={isCheckedPw} setIsCheckedPw={setIsCheckedPw} />
 
       <div className={styles.twoBtnBox}>
         <Button txt="돌아가기" onClick={() => setPage("login")} />
--- src/views/login/pageCompnents/CheckNickName.jsx
+++ src/views/login/pageCompnents/CheckNickName.jsx
@@ -6,35 +6,32 @@ import Txt from "../../../components/Txt";
 import Input from "../../../components/Input";
 import Button from "../../../components/Button";
 
-const CheckNickName = ( { nickName, setNickName, isCheckedNickName, setIsCheckedNickName, authService } ) => {
-
+const CheckNickName = ({ nickName, setNickName, isCheckedNickName, setIsCheckedNickName, authService }) => {
   const checkNickName = async () => {
-    console.log(nickName)
-    const checkNick = await authService.validateNickname(nickName)
-    setIsCheckedNickName(checkNick)
-  }
+    console.log(nickName);
+    const checkNick = await authService.validateNickname(nickName);
+    console.log(checkNick.data);
+    setIsCheckedNickName(checkNick);
+  };
 
-
-  return(
+  return (
     <>
       <Txt txt="닉네임" />
-        <div className={styles.inputAndBtn}>
-          <Input type="text" value={nickName} onChange={(e) => setNickName(e.target.value)} />
-          <Button txt="닉네임 중복검사" onClick={checkNickName} />
-        </div>
-        <Txt  
-          txt={
-              isCheckedNickName 
-              ? "닉네임 사용이 가능합니다"
-              : isCheckedNickName === null
-                ? null // 초기값만 아무것도 표시 안되게.
-                : "사용이 불가능한 닉네임입니다."
-            } 
-        />
+      <div className={styles.inputAndBtn}>
+        <Input type="text" value={nickName} onChange={(e) => setNickName(e.target.value)} />
+        <Button txt="닉네임 중복검사" onClick={checkNickName} />
+      </div>
+      <Txt
+        txt={
+          isCheckedNickName
+            ? "닉네임 사용이 가능합니다"
+            : isCheckedNickName === null
+            ? null // 초기값만 아무것도 표시 안되게.
+            : "사용이 불가능한 닉네임입니다."
+        }
+      />
     </>
-    
-  )
-  
-}
+  );
+};
 
 export default CheckNickName;
